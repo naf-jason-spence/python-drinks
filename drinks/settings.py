1a40a351f65861026372ee4c66d25c6cc8a69cfc
Jason Spence <jason.spence@nafinc.com>
2024-07-05 17:16:00 -0500

    settings
    settings

--- drinks/settings.py
+++ drinks/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles').replace('\\', '/')
 
 if 'RENDER' in os.environ:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
